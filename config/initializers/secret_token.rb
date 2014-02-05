06251f7add6bd640200a3904bc8143ae8cf16b9d
Edmund Ye <edmund.ye@airbnb.com>
2014-02-05 11:56:21 -0800

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '57bd6a4dab736264d9f87ef305e74e90012a53e167dfd1190c63dcda1fbd8ec57738668d56ab9264ef7d23839336f840afae132cf60e865609a252f906fd2bd7'
